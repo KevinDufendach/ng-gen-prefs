d500963dac6ec684b24a8d9c83e6c4b7b511c891
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-07-03 20:02:20 -0400

    allow wrap in field options

--- src/app/fields/fields.module.ts
+++ src/app/fields/fields.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FieldListComponent } from './field-list.component';
 import {FieldModule} from "../../../projects/ng-redcap/src/field/field.module";
+import {MatCardModule} from "@angular/material";
 
 @NgModule({
   declarations: [FieldListComponent],
@@ -10,7 +11,8 @@ import {FieldModule} from "../../../projects/ng-redcap/src/field/field.module";
   ],
   imports: [
     CommonModule,
-    FieldModule
+    FieldModule,
+    MatCardModule
   ]
 })
 export class FieldsModule { }
