10a99964969067bf02f72642810314a94eab8855
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-07-07 23:20:07 -0400

    create list of shown and not shown conditions

--- src/app/conditions-display/conditions-display.component.ts
+++ src/app/conditions-display/conditions-display.component.ts
@@ -1,11 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import {REDCapService} from '../../../projects/ng-redcap/src/field/redcap.service';
 import {ConditionsService} from '../conditions/conditions.service';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-conditions-display',
   templateUrl: './conditions-display.component.html',
-  styleUrls: ['./conditions-display.component.scss']
+  styleUrls: ['./conditions-display.component.scss'],
+  animations: [
+    trigger(
+      'expandAnimation', [
+        transition(':enter', [
+          style({transform: 'translateY(100%)', opacity: 0}),
+          animate('500ms', style({transform: 'translateY(0)', opacity: 1}))
+        ]),
+        transition(':leave', [
+          style({transform: 'translateY(0)', opacity: 1}),
+          animate('500ms', style({transform: 'translateY(100%)', opacity: 0}))
+        ])
+      ]
+    )
+  ]
 })
 export class ConditionsDisplayComponent implements OnInit {
 
