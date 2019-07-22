d26dfdbd4471d9390ea7c8c03e5a002be6d21ec0
Kevin Dufendach <Kevin.Dufendach@gmail.com>
2019-07-22 15:42:38 -0400

    update question stepper

--- src/app/entry-dialog/entry-dialog.component.ts
+++ src/app/entry-dialog/entry-dialog.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {MatDialogRef} from '@angular/material';
+import {REDCapService} from "../../../projects/ng-redcap/src/field/redcap.service";
 
 @Component({
   selector: 'app-entry-dialog',
@@ -8,7 +9,20 @@ import {MatDialogRef} from '@angular/material';
 })
 export class EntryDialogComponent implements OnInit {
 
-  constructor(public dialogRef: MatDialogRef<EntryDialogComponent>) { }
+  constructor(public dialogRef: MatDialogRef<EntryDialogComponent>, public rs: REDCapService) { }
+
+  manualFieldNames = [
+    'include_all_adol',
+    'preventable_adol',
+    'treatable_adol',
+    'adult_onset_adol',
+    'carrier_adol',
+    // 'parental_involve_adol',
+    // 'manual_include_adol',
+    // 'manual_exclude_adol',
+    // 'man_incl_car_adol',
+    // 'man_excl_car_adol',
+  ];
 
   ngOnInit() {
   }
